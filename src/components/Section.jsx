02efead81cb312337f40a9889c2a42d7ce026a34
Sara Vanessa <Saraguedes100@gmail.com>
2024-07-26 10:06:40 -0300

    Resolvendo alinhamento dos itens

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
 const Section = ({ children, title}) => {
   return (
-    <div className="w-4/5">
+    <div className="flex items-center flex-col">
       <div className="flex max-w-[1440px]" >
         <h2 className="pb-3 pt-1 text-black font-bold font-oswald text-3xl">{title}</h2>
 
